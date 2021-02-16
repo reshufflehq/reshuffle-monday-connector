81e747b9a190edb5f776fd5eb4ab46fe4b7f48b0
christophe gachiniard <christophe@reshuffle.com>
2021-02-16 14:59:17 +1300

    create item with column values in a single transaction

--- src/index.ts
+++ src/index.ts
@@ -315,6 +315,27 @@ export default class MondayConnector extends BaseHttpConnector<
     return obj
   }
 
+  private columnValueUpdatersToObject(
+    columnValues: Record<string, (value?: any) => any>,
+    boardColumns: Record<string, any>,
+  ): Record<string, any> {
+    const newValues: any = {}
+    for (const [key, updater] of Object.entries(columnValues)) {
+      const bc = boardColumns.find(({ title }) => title === key)
+      if (!bc) {
+        throw new Error(`Column title not found: ${key}`)
+      }
+
+      if (typeof updater !== 'function') {
+        throw new Error(`Missing or invalid updater for: ${key}`)
+      }
+      const newValue = updater()
+      newValues[bc.id] = typeof newValue !== 'object' ? String(newValue) : newValue
+    }
+
+    return newValues
+  }
+
   getBoard(boardIds: number | number[]): Promise<MondayApiResponse['data']> {
     return this.query(GET_BOARD_QUERY, { board_ids: boardIds })
   }
@@ -404,15 +425,20 @@ export default class MondayConnector extends BaseHttpConnector<
     columnValues?: Record<string, (value: string) => any>,
     groupId?: string,
   ): Promise<string> {
+    const boardColumnsResp = await this.getColumn(boardId)
+    const boardColumns = boardColumnsResp?.boards[0].columns
+
     const res = await this.query(CREATE_ITEM_QUERY, {
       board_id: boardId,
       group_id: groupId,
       item_name: itemName,
+      column_values:
+        columnValues && boardColumns
+          ? JSON.stringify(this.columnValueUpdatersToObject(columnValues, boardColumns))
+          : undefined,
     })
     const itemId: string = (res as any).create_item.id
-    if (columnValues) {
-      this.updateColumnValues(boardId, itemId, columnValues)
-    }
+
     return itemId
   }
 
